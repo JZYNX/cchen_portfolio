52e78e64b932697376ee9449e6b9dba6ec6ebf95
jz <jasontang02@outlook.com>
2025-05-25 21:00:06 +1000

    update root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function HomePage() {
   return (
     <div>
-      
+      <h2 className="text-2xl font-semibold mb-4">Hi, I’m JZ 👋</h2>
+      <p className="text-lg">
+        I'm a software engineer passionate about building cool things with code.
+        Check out my projects and get in touch!
+      </p>
     </div>
   );
 }
